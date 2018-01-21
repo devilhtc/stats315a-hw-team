9ffb71d7a3924c3cc3fced947489be24853f7d28
ziyi <yangziyi99@gmail.com>
2018-01-20 20:31:25 -0800

    add function to generate centroids and test/training data

--- hw1/q1.py
+++ hw1/q1.py
@@ -5,11 +5,12 @@ def main():
 	Util = util.Util
 	u = Util()
 	testUtil(u)
-	
-def testUtil(u):
-	print('hello world!')
-	print(u.test())
 
+def testUtil(u):
+	#print('hello world!')
+	c_0, c_1 = u.gen_cent()
+	data = u.gen_data(c_0, 100)
+	print (data)
 
 if __name__=='__main__':
 	main()
--- hw1/util.py
+++ hw1/util.py
@@ -1,6 +1,31 @@
+import numpy as np
+import random
 
 class Util():
 	def test(self):
 		return 'testing complete'
+	def gen_cent(self):
+		"""
+		Generate 10 centroids for each class
+		"""
+		#mean and conv for class 0
+		mean_0 = (1, 0); cov_0 = [[1, 0], [0, 1]]
+		c_0 = np.random.multivariate_normal(mean_0, cov_0, (10, ))
 
-	
+		mean_1 = (0, 1); cov_1 = [[1, 0], [0, 1]]
+		c_1 = np.random.multivariate_normal(mean_1, cov_1, (10, ))
+		return (c_0, c_1)
+
+	def gen_data(self, cen, num_data):
+		'''
+		#cen are centroids
+		#num_data is the number of datasets
+		return result (num_data, 2)
+		'''
+		result = []
+		for _ in range(num_data):
+			rand_index = random.randint(0, 9)
+			mean = cen[rand_index, :]; cov = [[1/5, 0], [0, 1/5]]
+			data = np.random.multivariate_normal(mean, cov)
+			result.append(data)
+		return np.stack(result)
