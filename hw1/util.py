072022c173a7a226cbde4dd52e0bfa5af6790372
devilhtc <devilmengsk@gmail.com>
2018-01-20 20:39:34 -0800

    rearranged some comments

--- hw1/util.py
+++ hw1/util.py
@@ -5,10 +5,10 @@ class Util():
 	def test(self):
 		return 'testing complete'
 
-	def genCent(self):
-		"""
+	'''
 		Generate 10 centroids for each class
-		"""
+	'''
+	def genCent(self):
 		#mean and conv for class 0
 		mean_0 = (1, 0); cov_0 = [[1, 0], [0, 1]]
 		c_0 = np.random.multivariate_normal(mean_0, cov_0, (10, ))
@@ -16,13 +16,14 @@ class Util():
 		mean_1 = (0, 1); cov_1 = [[1, 0], [0, 1]]
 		c_1 = np.random.multivariate_normal(mean_1, cov_1, (10, ))
 		return (c_0, c_1)
-
-	def genData(self, cen, num_data):
-		'''
-		#cen are centroids
-		#num_data is the number of datasets
+		
+	'''
+		cen are centroids
+		num_data is the number of datasets
 		return result (num_data, 2)
-		'''
+	'''
+	def genData(self, cen, num_data):
+		
 		result = []
 		for _ in range(num_data):
 			rand_index = random.randint(0, 9)
