b32bac831a5f49343df80c0e46a0a235e545f517
devilhtc <devilmengsk@gmail.com>
2018-02-12 23:04:23 -0800

    renamed util and added output for each part

--- hw2q4/q4.py
+++ hw2q4/q4.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import util as u
+import q4_util as u
 
 np.random.seed(315)
 
@@ -96,15 +96,22 @@ def get_all_data():
 '''                      '''
 
 def partF():
+
+	print 'part F'
+
+	print 'getting data...'
 	X, y, _, _ = get_all_data()
 	n, p = X.shape
 	steps = np.arange(p) + 1
 
 	# train and test model
+	print 'training and testing model...'
 	model = u.train_model(X, y)
 	y_hat = model.predict_at_all_steps(X)
 	y_hat_binary = (y_hat > 0.5)
 	y_augmented = np.repeat(y, p, axis = 1)
+
+	print 'making plots...'
 
 	# 1. plot rss vs step
 	rss = np.sum(np.power(y_hat - y_augmented, 2), axis = 0)
@@ -122,8 +129,14 @@ def partF():
 	plt.ylabel('misclassification error')
 	plt.savefig('partF2.png')
 
+	print 'saved'
+	print 
+
 
 def partG():
+	print 'part G'
+	print 'getting data and dividing into folds...'
+
 	# get data and fold indices
 	X, y, _, _ = get_all_data()
 	n, p = X.shape
@@ -133,7 +146,9 @@ def partG():
 	# aggregate error at each fold
 	fold_errors = []
 
-	for fold_idx in fold_idxs:
+	print 'training and testing on the folds...'
+	for i in range(num_folds):
+		fold_idx = fold_idxs[i]
 		# get train and test for this fold
 		X_train, y_train, X_test, y_test = u.get_fold_Xys(X, y, fold_idx)
 		n_test = X_test.shape[0]
@@ -146,6 +161,10 @@ def partG():
 		y_augmented = np.repeat(y_test, p, axis = 1)
 		err_arr = np.mean( np.abs(y_test_hat_binary - y_augmented), axis = 0)
 		fold_errors.append(err_arr)
+
+		print 'fold {0} done'.format(i + 2)
+
+	print 'analyzing data and making plots...'
 
 	# get the mean and std for all folds at each step
 	fold_errors_array = np.array(fold_errors)
@@ -161,19 +180,30 @@ def partG():
 	plt.legend()
 	plt.savefig('partG.png')	
 
+	print 'saved'
+	print
+
 def partH():
+	print 'part H'
+	print 'getting data...'
+
 	# with 28 chosen 
 	chosen = 28
+	print 'we chose to take {0} steps based on previous results'.format(chosen)
+
 	X_train, y_train, X_test, y_test = get_all_data()
 	n, p = X_train.shape
 	steps = np.arange(p) + 1
 
+	print 'training and testing model...'
 	model = u.train_model(X_train, y_train)
 	y_test_hat = model.predict_at_all_steps(X_test)
 	y_test_hat_binary = y_test_hat > 0.5
 
 	y_augmented = np.repeat(y_test, p, axis = 1)
 	err_arr = np.mean( np.abs(y_test_hat_binary - y_augmented), axis = 0)
+
+	print 'making plot...'
 
 	plt.figure('h')
 	plt.plot(steps, err_arr, label = 'test error')
@@ -183,19 +213,35 @@ def partH():
 	plt.legend()
 	plt.savefig('partH.png')
 
+	print 'saved'
+	print
+
 def partI():
+	print 'part I'
+	print 'getting data...'
 	X_train, y_train, X_test, y_test = get_all_data()
 	n, p = X_train.shape
 	steps = np.arange(p) + 1
 
+	print 'training model and extract coefficients...'
 	model = u.train_model(X_train, y_train)
 	
+	print 'analyzing data...'
 	# get coefficients
 	cm = model.get_coefficient_matrix()
 	selected_idx = model.selected_idx
-
 	intercepts = u.prepend(cm[0, :], np.mean(y_test))
 	intercepts_steps = np.arange(p + 1)
+
+	# calculate R squared
+	y_train_hat = model.predict_at_all_steps(X_train)
+	y_train_augmented = np.repeat(y_train, p, axis = 1)
+	ss_res = np.sum(np.power(y_train_hat - y_train_augmented, 2), axis = 0)
+	ss_tot = np.sum( np.power(y_train - np.mean(y_train), 2) )
+	R_squared = 1.0 - ss_res / ss_tot
+	R_squared_with_null = u.prepend(R_squared, 0.0)
+
+	print 'making plots...'
 
 	# plot the coefficients path vs step
 	plt.figure('i1')
@@ -211,14 +257,6 @@ def partI():
 	plt.legend()
 	plt.savefig('partI1.png')
 
-	# calculate R squared
-	y_train_hat = model.predict_at_all_steps(X_train)
-	y_train_augmented = np.repeat(y_train, p, axis = 1)
-	ss_res = np.sum(np.power(y_train_hat - y_train_augmented, 2), axis = 0)
-	ss_tot = np.sum( np.power(y_train - np.mean(y_train), 2) )
-	R_squared = 1.0 - ss_res / ss_tot
-	R_squared_with_null = u.prepend(R_squared, 0.0)
-
 	# plot the coefficients path vs R squared
 	plt.figure('i2')
 	plt.xlabel('R^2')
@@ -231,14 +269,17 @@ def partI():
 		else:
 			plt.plot(R_squared_with_null, u.prepend(cm[idx+1, :], 0.0))
 	plt.legend()
-	plt.savefig('partI2.png')	
+	plt.savefig('partI2.png')
 
+	print 'saved'
+	print 	
 
 def main():
 	#test_folds()
+	partF()
+	partG()
+	partH()
 	partI()
-
-
 
 if __name__=='__main__':
 	main()
