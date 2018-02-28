26e02e770f2ef54b2669058eeb8dc8f0c03a1406
devilhtc <devilmengsk@gmail.com>
2018-02-27 20:43:07 -0800

    wrote framework for data preprocessing

--- hw3/q6.py
+++ hw3/q6.py
@@ -5,20 +5,57 @@ import utils as u
 # constants
 data_dir = 'data/'
 train_filename = 'loan_train.csv'
-test_filename = 'load_test.csv'
+test_filename = 'loan_testx.csv'
 
-def print_train_values():
+
+def get_ranges():
+	train_range = u.get_file_ranges(data_dir + train_filename)
+	test_range = u.get_file_ranges(data_dir + test_filename)
+	p1 = len(train_range)
+	p2 = len(test_range)
+
+	for i in range(p2):
+		print('train', train_range[i+1])
+		print('test', test_range[i])
+		print()
+	print(p1, p2)
+	return train_range, test_range
+
+def get_non_float_ranges():
+	train_range, test_range = get_ranges()
+	p1 = len(train_range)
+	p2 = len(test_range)
+	lines = []
+	for i in range(p2):
+		if test_range[i][1] != 'float':
+			lines.append((test_range[i][0], test_range[i][2].union(train_range[i+1][2])))
+	u.write_lines(lines, data_dir + 'non_float_ranges.txt')
+
+# return X_train (n, p'), Y_train (n, 1), X_test (m, p')
+# X_train and X_test are processed in the same way
+def get_data():
 	train_lines = u.get_lines(data_dir + train_filename)
-	dim_names = u.split_line(train_lines[0])
-	dim_ranges, dim_types = u.summarize_range(train_lines[1:])
+	names = u.split_line(train_lines[0])
+	train_all = u.process_lines_by_name(train_lines[1:], names)
+	
+	test_lines = u.get_lines(data_dir + test_filename)
+	test_all = u.process_lines_by_name(test_lines[1:], names[1:])
 
-	train_ranges = zip(dim_names, dim_types, dim_ranges)
-	for r in train_ranges:
-		print(r)
+
+	train_XY = np.array(train_all)
+	X_train = train_XY[:, 1:]
+	Y_train = train_XY[:, :1]
+	X_test = np.array(test_all)
+
+	print('X_train shape', X_train.shape)
+	print('Y_train shape', Y_train.shape)
+	print('X_test shape', X_test.shape)
+	return X_train, Y_train, X_test
+
 
 def main():
-	print_train_values()
-	print('do we use python?')
+	get_data()
+	#print('do we use python?')
 	
 if __name__ == '__main__':
 	main()
--- hw3/utils.py
+++ hw3/utils.py
@@ -3,6 +3,58 @@ import numpy as np
 import unittest
 
 
+# process a value by its name, returns a list of values
+# e.g. name: age, 18 -> [18],  or name: reason, 'travel': [0, 1, 0, 0, 0, 0]
+def process_val_by_name(val, name):
+	non_float_range_dict = {
+		'employment': ['NA', '< 1', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10+'],
+		'status': ['unchecked', 'partial', 'checked'],
+		'reason': ['business', 'cc', 'moving', 'solar', 'home', 'educ', 'renovation', 'boat', 'debt', 'medical', 'event', 'other', 'transport', 'holiday'],
+		'quality': ['q1', 'q2', 'q3', 'q4', 'q5', 'q5', 'q6', 'q7'],
+		'initial_list_status': ['a', 'b'],
+		'term': [' 3 yrs', ' 5 yrs']
+	}
+
+	# convert a name to a one-hot vector with 1 at its place
+	def one_hot(name, name_list):
+		return [(1.0 if ele == name else 0.0) for ele in name_list]
+
+	# defines how we process the data
+	process_dict = {
+		'initial_list_status': lambda x: [1.0] if x == 'a' else [0.0],
+		'term': lambda x: [1.0] if '5' in x else [0.0],
+		'quality': lambda x: [float(x[1:])],
+		'employment': lambda x: one_hot(x, non_float_range_dict['employment']),
+		'reason': lambda x: one_hot(x, non_float_range_dict['reason']),
+		'status': lambda x: one_hot(x, non_float_range_dict['status']),
+	}
+
+	def default_process_func(x):
+		return [float(x)]
+
+	process_func = process_dict.get(name, default_process_func)
+	return process_func(val)
+
+# process the lines by its identifying names
+# return a list of list of floats for analysis
+def process_lines_by_name(lines, names):
+	processed_lines = []
+	for line in lines:
+		cur_line = []
+		splitted_line = split_line(line)
+		for i in range(len(names)):
+			cur_line = cur_line + process_val_by_name(splitted_line[i], names[i])
+		processed_lines.append(cur_line)
+	return processed_lines
+
+# get range of data stored in the file
+def get_file_ranges(filename):
+	lines = get_lines(filename)
+	dim_names = split_line(lines[0])
+	dim_ranges, dim_types = summarize_range(lines[1:])
+	train_ranges = zip(dim_names, dim_types, dim_ranges)
+	return train_ranges
+
 # split a line by ',' then strip out the '"'s
 def split_line(line):
 	return [ele.strip('"') for ele in line.split(',')]
@@ -15,7 +67,7 @@ def isfloat(value):
 	except ValueError:
 		return False
 
-# get stripped lines from a file
+# get stripped lines from a file 
 def get_lines(filename):
 	f = open(filename, 'r')
 	lines = []
@@ -23,6 +75,12 @@ def get_lines(filename):
 		lines.append(line.strip())
 	f.close()
 	return lines
+
+def write_lines(lines, filename):
+	f = open(filename, 'w+')
+	for line in lines:
+		f.write(str(line) + '\n')
+	f.close()
 
 # return 'float' if all the strings can be convereted to float, otherwise return 'string'
 def get_types(set_of_values):
@@ -61,6 +119,9 @@ class UtilTests(unittest.TestCase):
 		vs, ts = summarize_range(self.s_lines)
 		self.assertEqual(vs, self.s_values, 'summarize_range values failed')
 		self.assertEqual(ts, self.s_types, 'summarize_range types failed')
+
+	def test_process(self):
+		pass
 
 def main():
 	unittest.main()
