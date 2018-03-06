5a5a50781314304f382ebc48adf3be8f8ebfb3bc
ziyi <yangziyi99@gmail.com>
2018-03-05 19:50:40 -0800

    add a new file ss.py and update models.py

--- hw3/models.py
+++ hw3/models.py
@@ -8,9 +8,9 @@ models.py
 
 file for statistic learning models
 
-each model 
+each model
 1. should implement the object BasicModel, it should have at least 3 more methods: fit, predict and get_params
-2. should be initiazed with a set a parameters 
+2. should be initiazed with a set a parameters
 3. should be given X (n, p), Y (n, 1) when fitting
 4. should be given X (n, p) when testing
 5. should return a dictionary mapping name of the parameter to its value
@@ -44,7 +44,7 @@ class SKLinearModel(BasicModel):
 		self.fitted = True
 		beta = np.concatenate([self.model.intercept_, self.model.coef_.flatten()], axis = 0).reshape((-1, 1))
 		self.params['beta'] = beta
-		
+
 	def predict(self, X):
 		if not self.fitted:
 			raise Exception('This model was not yet fitted')
@@ -59,7 +59,7 @@ class SKLinearModel(BasicModel):
 class LinearModel(BasicModel):
 	def __init__(self):
 		self.fitted = False
-		self.beta = None 
+		self.beta = None
 		self.params = {}
 
 	def fit(self, X, Y):
@@ -77,7 +77,7 @@ class LinearModel(BasicModel):
 			raise Exception('This model was not yet fitted')
 		return self.params
 
-# ridge regression model 
+# ridge regression model
 # alpha is the penalty on the l2 norm^2 of weights (default 1.0)
 class RidgeModel(BasicModel):
 	def __init__(self, alpha = 1.0):
@@ -90,7 +90,7 @@ class RidgeModel(BasicModel):
 		self.fitted = True
 		beta = np.concatenate([self.model.intercept_, self.model.coef_.flatten()], axis = 0).reshape((-1, 1))
 		self.params['beta'] = beta
-		
+
 	def predict(self, X):
 		if not self.fitted:
 			raise Exception('This model was not yet fitted')
@@ -101,7 +101,7 @@ class RidgeModel(BasicModel):
 			raise Exception('This model was not yet fitted')
 		return self.params
 
-# lasso model 
+# lasso model
 # alpha is the penalty on the l1 norm weights (default 1.0)
 class LassoModel(BasicModel):
 	def __init__(self, alpha = 1.0):
@@ -114,7 +114,7 @@ class LassoModel(BasicModel):
 		self.fitted = True
 		beta = np.concatenate([self.model.intercept_, self.model.coef_.flatten()], axis = 0).reshape((-1, 1))
 		self.params['beta'] = beta
-		
+
 	def predict(self, X):
 		if not self.fitted:
 			raise Exception('This model was not yet fitted')
@@ -125,7 +125,7 @@ class LassoModel(BasicModel):
 			raise Exception('This model was not yet fitted')
 		return self.params
 
-# elastic net model 
+# elastic net model
 # alpha is total penalty
 # lam is the proportion on l1 norm
 class ElasticNetModel(BasicModel):
@@ -139,7 +139,7 @@ class ElasticNetModel(BasicModel):
 		self.fitted = True
 		beta = np.concatenate([self.model.intercept_, self.model.coef_.flatten()], axis = 0).reshape((-1, 1))
 		self.params['beta'] = beta
-		
+
 	def predict(self, X):
 		if not self.fitted:
 			raise Exception('This model was not yet fitted')
@@ -157,7 +157,7 @@ class TestModels(unittest.TestCase):
 		self.X_train = np.array([[1.0, 0.0], [0.0, 1.0], [0.0, 0.0]])
 		self.Y_train = np.array([[1.0], [2.0], [-1.0]])
 		self.X_test = np.array([ [1.0, 1.0], [1.5, 1.0]])
-		
+
 	def test_not_fitted(self):
 		lm = SKLinearModel()
 		try:
@@ -165,7 +165,7 @@ class TestModels(unittest.TestCase):
 			self.assertEqual(0, 1, msg = 'SKLinearModel able to predict without fitting')
 		except:
 			self.assertEqual(0, 0, msg = 'Allisgud')
-			
+
 	def test_basic_model(self):
 		try:
 			b = BasicModel()
@@ -177,7 +177,7 @@ class TestModels(unittest.TestCase):
 		X_train = self.X_train
 		Y_train = self.Y_train
 		X_test = self.X_test
-		
+
 		lm = SKLinearModel()
 		lm.fit(X_train, Y_train)
 
@@ -186,7 +186,7 @@ class TestModels(unittest.TestCase):
 		X_train_augmented = u.augment(X_train)
 		beta = u.linear_reg(X_train_augmented, Y_train)
 		Y_test_true = u.augment(X_test).dot(beta)
-		
+
 		m = Y_test.shape[0]
 		for i in range(m):
 			self.assertEqual(int(Y_test[i, 0]), int(Y_test_true[i, 0]), msg = 'SKLinearModel coefficients wrong')
---
+++ hw3/ss.py
@@ -0,0 +1,8 @@
+from sklearn.feature_selection import VarianceThreshold
+
+class featureSelection():
+    def var_threshold():
+
+    def l1_selection():
+
+    
