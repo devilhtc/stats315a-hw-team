424d0329b6d7423d6887eca700fd7ab5fd1f0a05
ziyi <yangziyi99@gmail.com>
2018-03-16 00:43:53 -0700

    add gaussian interval

--- hw3/interval.py
+++ hw3/interval.py
@@ -2,6 +2,7 @@ import numpy as np
 from q6 import get_data
 import sklearn.linear_model as sklm
 from sklearn import svm
+from scipy import stats
 from utils import *
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
@@ -17,14 +18,17 @@ def preprocess(X):
     scaler = StandardScaler().fit(X)
     return scaler.transform(X)
 
-def mean_confidence_interval(data, confidence=0.95):
+def mean_confidence_interval(data, confidence=0.90):
     a = 1.0*np.array(data)
     n = len(a)
     m, se = np.mean(a), scipy.stats.sem(a)
     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
     return m, m-h, m+h
 
-
+def gaussian_confidence_interval(data, confidence = 0.9):
+    N = data.shape[0]
+    mu, sigma = np.mean(data), np.std(data)
+    return stats.norm.interval(confidence, loc=mu, scale=sigma/np.sqrt(N))
 
 def main():
     X_train, y_train, X_test = get_data()
@@ -34,7 +38,7 @@ def main():
 
     model.fit(X_train, y_train)
     scores = model.predict(X_test)
-    print(mean_confidence_interval(scores))
+    print(gaussian_confidence_interval(scores))
 
 if __name__ == '__main__':
     main()
